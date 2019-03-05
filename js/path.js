b632ef44bf3e6f3d6e789837430e4667b7c8271c
luuuuuuuyouli <15826383768@163.com>
2019-03-05 22:48:38 +0800

    初始提交

---
+++ js/path.js
@@ -0,0 +1,9 @@
+/**
+ * get path
+ */
+function getPath() {
+   // return "http://192.168.0.151:8080/liliao";
+//      return"http://120.27.104.73/liliao";
+    return"http://api.tongyude.cn/liliao";
+}
+
